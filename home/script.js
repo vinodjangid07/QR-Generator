46f022c287396f6243c3948975e1e25594311039
vinod <infovinodjangid@gmail.com>
2023-03-22 20:22:22 +0530

    File Download Format Added

--- home/script.js
+++ home/script.js
@@ -14,18 +14,15 @@
 let QrContainer = document.getElementById('Qr-container');
 let exit = document.getElementById('exit');
 let QR = document.getElementById('qr-code');
-
-exit.addEventListener('click',()=>{
-    QrContainer.classList.remove('exit-Qr');
-    loader.classList.remove('deactivate-loader');
-})
-
-
-
 let input = document.getElementById('input');
 let submit = document.getElementById('submit');
+var loader = document.getElementById("loader");
+let jpg = document.getElementById('forjpg');
+let png = document.getElementById('forpng');
+let svg = document.getElementById('forsvg');
 
 
+// submit using click
 submit.addEventListener('click',()=>{
     let fetchedData = input.value;
 
@@ -46,61 +43,83 @@ submit.addEventListener('click',()=>{
     }
   
 
-})
+});
+QR.addEventListener("load", function () {
+  
+    loader.classList.add('deactivate-loader');
 
+});
 
-// ==============================================
-let btnDownload = document.getElementById('download');
+// submit using enter press
 
-btnDownload.addEventListener('click',()=>{
-let imgPath = QR.getAttribute('src');
+document.body.addEventListener('keypress', (e) =>{
+    if(e.key == 'Enter'){
+        let fetchedData = input.value;
+
+        if (input.value.length == 0) {
+            input.classList.add('shake-horizontal');
+            setTimeout(function() {
+                input.classList.remove('shake-horizontal');
+            }, 500);
+            vibratePhone();
+            // vibration function called
+        } 
+        
+        
+        else {
+            QrContainer.classList.add('exit-Qr');
+            let apiLink = `https://api.qrserver.com/v1/create-qr-code/?size=250x250&margin=30&data=${fetchedData}`;
+            QR.src = apiLink;
+        }
+      
+    }
+});
+
+// vibration if input field is empty
+function vibratePhone() {
+     if (navigator.vibrate) {
+    navigator.vibrate(500);
+    } else {
+    console.log("Vibration API is not supported in your browser.");
+    }
+    };
+
+// ====================================================================================================
+// download button
+
+jpg.addEventListener('click',()=>{
+let imgPath = QR.getAttribute('src')+`&format=jpeg`;
 let fileName = "QR CODE";
 
 
 saveAs(imgPath, fileName);
 });
 
-// function getFileName(str){
-//     return str.substring(str.lastIndexOf('/')+1);
-// }
+png.addEventListener('click',()=>{
+    let imgPath = QR.getAttribute('src')+`&format=png`;
+    let fileName = "QR CODE";
+    
+    
+    saveAs(imgPath, fileName);
+    });
+    
 
-var loader = document.getElementById("loader");
-QR.addEventListener("load", function () {
-  
-        loader.classList.add('deactivate-loader');
-  
-  
-  
-})
-
-function vibratePhone() { if (navigator.vibrate) {
-    navigator.vibrate(500);
-    } else {
-    console.log("Vibration API is not supported in your browser.");
-    }
-    }
+    svg.addEventListener('click',()=>{
+        let imgPath = QR.getAttribute('src')+`&format=svg`;
+        let fileName = "QR CODE";
+        
+        
+        saveAs(imgPath, fileName);
+        });
+        
+        
 
 
-    document.body.addEventListener('keypress', (e) =>{
-        if(e.key == 'Enter'){
-            let fetchedData = input.value;
 
-            if (input.value.length == 0) {
-                input.classList.add('shake-horizontal');
-                setTimeout(function() {
-                    input.classList.remove('shake-horizontal');
-                }, 500);
-                vibratePhone();
-                
-            } 
-            
-            
-            else {
-                QrContainer.classList.add('exit-Qr');
-                let apiLink = `https://api.qrserver.com/v1/create-qr-code/?size=250x250&margin=30&data=${fetchedData}`;
-                QR.src = apiLink;
-            }
-          
-        }
-    })
- 
+
+
+    //exit button  
+exit.addEventListener('click',()=>{
+    QrContainer.classList.remove('exit-Qr');
+    loader.classList.remove('deactivate-loader');
+});
